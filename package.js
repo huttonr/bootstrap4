3a981cbfaa5ebcd4d4e355834ccacf6f677b3d5d
Carl Lange <carl@flax.ie>
2016-07-12 20:44:28 +0100

    update babel from 5.x to 6.x

--- package.js
+++ package.js
@@ -2,7 +2,7 @@
 Package.describe({
   name: 'huttonr:bootstrap4',
   summary: 'Modular, customizable Bootstrap 4.',
-  version: '4.0.0_13',
+  version: '4.0.0_14',
   git: 'https://github.com/huttonr/bootstrap4',
   documentation: 'README.md'
 });
@@ -18,8 +18,8 @@ Package.registerBuildPlugin({
   name: 'build-bootstrap4',
   use: [
     'underscore@1.0.4',
-    'ecmascript@0.1.6',
-    'babel-compiler@5.8.24_1',
+    'ecmascript',
+    'babel-compiler',
     'huttonr:bootstrap4-assets@4.0.0_9'
   ],
   sources: [
