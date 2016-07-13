7773ca7f929eb88174c956e752beebe8177db0b6
HuttonR <hutton.rounds@thebronson.com>
2016-07-12 18:49:25 -0700

    Upgraded to latest Babel

--- package.js
+++ package.js
@@ -2,13 +2,13 @@
 Package.describe({
   name: 'huttonr:bootstrap4',
   summary: 'Modular, customizable Bootstrap 4.',
-  version: '4.0.0_14',
+  version: '4.0.0_15',
   git: 'https://github.com/huttonr/bootstrap4',
   documentation: 'README.md'
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.2.1');
+  api.versionsFrom('1.3.4.4');
   api.use('isobuild:compiler-plugin@1.0.0');
 
   api.addFiles('check-settings.js', 'client');
@@ -18,8 +18,8 @@ Package.registerBuildPlugin({
   name: 'build-bootstrap4',
   use: [
     'underscore@1.0.4',
-    'ecmascript',
-    'babel-compiler',
+    'ecmascript@0.4.7',
+    'babel-compiler@6.8.4',
     'huttonr:bootstrap4-assets@4.0.0_9'
   ],
   sources: [
